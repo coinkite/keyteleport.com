d439f5532db57cf7879665600458f158681bb77a
siim-m <46551195+siim-m@users.noreply.github.com>
2025-03-21 14:54:40 +0000

    first pass: splitting controls

--- js/main.js
+++ js/main.js
@@ -2,11 +2,14 @@
 /// <reference path="bbqr.d.ts" />
 
 (async function () {
-  /** @typedef {{ status: 'loading' }}  Loading */
-  /** @typedef {{ status: 'invalid-hash' }} InvalidHash */
-  /** @typedef {{ status: 'no-hash' }} NoHash */
-  /** @typedef {{ status: 'qr-code', url: string, data: string }} QRCode */
-  /** @typedef {(Loading | InvalidHash | NoHash | QRCode)}  PageState*/
+  /** @typedef {{qrParts: string[]; rawBytes: Uint8Array; encoding: '2' | 'Z'; fileType: 'S' | 'R' | 'E'; qrVersion: number}} QRInfo */
+
+  /** @typedef {{ status: 'loading' }}  LoadingState */
+  /** @typedef {{ status: 'invalid-hash' }} InvalidHashState */
+  /** @typedef {{ status: 'no-hash' }} NoHashState */
+  /** @typedef {{ status: 'loaded'; qrInfo: QRInfo }} LoadedState */
+
+  /** @typedef {(LoadingState | InvalidHashState | NoHashState | LoadedState)}  PageState*/
 
   /**
    * @template {any} T
@@ -14,7 +17,7 @@
    */
   function mustExist(el) {
     if (el === null || el === undefined) {
-      alert('Missing HTML element(s) on page!');
+      alert('Missing HTML element(s) on page');
       throw new Error();
     }
     return el;
@@ -26,87 +29,120 @@
   const qrCodeEl = mustExist(document.querySelector('.qr-code'));
   const qrActionButtonsEl = mustExist(document.querySelector('.qr-action-buttons'));
   const copyToastEl = mustExist(document.querySelector('.copy-toast'));
+  const countControlsEl = mustExist(document.querySelector('.count-controls'));
+  // const countInputEl = /** @type {HTMLInputElement} */ (
+  //   mustExist(document.querySelector('.qr-count-input'))
+  // );
 
   const DATA_REGEX = /^B\$[2Z][SRE][0-9A-Z]{2}[0-9A-Z]{2}[2-7A-Z]+$/;
 
-  /**
-   * @param {PageState} state
-   */
-  function updatePageState(state) {
-    const currentImg = qrCodeEl.querySelector('img');
+  const { getState, setState } = (function () {
+    /** @type {PageState} */
+    let _state = { status: 'loading' };
 
-    if (currentImg) {
-      URL.revokeObjectURL(currentImg.src);
-      currentImg.remove();
+    /**
+     * @param {PageState} newState
+     */
+    async function setState(newState) {
+      const currentImg = qrCodeEl.querySelector('img');
+      if (currentImg) {
+        URL.revokeObjectURL(currentImg.src);
+        currentImg.remove();
+      }
+      switch (newState.status) {
+        case 'loading':
+          spinnerEl.classList.remove('hidden');
+          infoEl.classList.add('hidden');
+          errorEl.classList.add('hidden');
+          qrCodeEl.classList.add('hidden');
+          qrActionButtonsEl.classList.add('hidden');
+          break;
+        case 'invalid-hash':
+          spinnerEl.classList.add('hidden');
+          infoEl.classList.add('hidden');
+          errorEl.classList.remove('hidden');
+          qrCodeEl.classList.add('hidden');
+          qrActionButtonsEl.classList.add('hidden');
+          break;
+        case 'no-hash':
+          spinnerEl.classList.add('hidden');
+          infoEl.classList.remove('hidden');
+          errorEl.classList.add('hidden');
+          qrCodeEl.classList.add('hidden');
+          qrActionButtonsEl.classList.add('hidden');
+          break;
+        case 'loaded':
+          const imgBuf = await BBQr.renderQRImage(
+            newState.qrInfo.qrParts,
+            newState.qrInfo.qrVersion,
+            {
+              renderOptions: {
+                scale: 8,
+              },
+            }
+          );
+
+          const imgUrl = URL.createObjectURL(new Blob([imgBuf], { type: 'image/png' }));
+
+          spinnerEl.classList.add('hidden');
+          infoEl.classList.add('hidden');
+          errorEl.classList.add('hidden');
+
+          if (newState.qrInfo.rawBytes.length < 200) {
+            countControlsEl.classList.add('hidden');
+          } else {
+            countControlsEl.classList.remove('hidden');
+          }
+
+          if (newState.qrInfo.qrParts.length === 1) {
+            countControlsEl
+              .querySelector('[data-action="no-animation"]')
+              ?.setAttribute('disabled', '');
+            countControlsEl
+              .querySelector('[data-action="less-frames"]')
+              ?.setAttribute('disabled', '');
+            countControlsEl
+              .querySelector('[data-action="more-frames"]')
+              ?.removeAttribute('disabled');
+          } else {
+            countControlsEl
+              .querySelector('[data-action="no-animation"]')
+              ?.removeAttribute('disabled');
+            countControlsEl
+              .querySelector('[data-action="less-frames"]')
+              ?.removeAttribute('disabled');
+            countControlsEl
+              .querySelector('[data-action="more-frames"]')
+              ?.removeAttribute('disabled');
+          }
+
+          qrCodeEl.classList.remove('hidden');
+          qrActionButtonsEl.classList.remove('hidden');
+          qrCodeEl.innerHTML = `<img src="${imgUrl}" alt="BBQr code">`;
+
+          break;
+        default:
+          /** @type {never} */
+          const _exhaustiveCheck = newState;
+      }
+      _state = newState;
+      console.debug('setState', newState);
     }
-
-    switch (state.status) {
-      case 'loading':
-        spinnerEl.classList.remove('hidden');
-        infoEl.classList.add('hidden');
-        errorEl.classList.add('hidden');
-        qrCodeEl.classList.add('hidden');
-        qrActionButtonsEl.classList.add('hidden');
-        break;
-      case 'invalid-hash':
-        spinnerEl.classList.add('hidden');
-        infoEl.classList.add('hidden');
-        errorEl.classList.remove('hidden');
-        qrCodeEl.classList.add('hidden');
-        qrActionButtonsEl.classList.add('hidden');
-        break;
-      case 'no-hash':
-        spinnerEl.classList.add('hidden');
-        infoEl.classList.remove('hidden');
-        errorEl.classList.add('hidden');
-        qrCodeEl.classList.add('hidden');
-        qrActionButtonsEl.classList.add('hidden');
-        break;
-      case 'qr-code':
-        spinnerEl.classList.add('hidden');
-        infoEl.classList.add('hidden');
-        errorEl.classList.add('hidden');
-        qrCodeEl.classList.remove('hidden');
-        qrActionButtonsEl.classList.remove('hidden');
-        qrCodeEl.innerHTML = `<img src="${state.url}" alt="BBQr code" data-data="${state.data}" />`;
-
-        break;
-      default:
-        /** @type {never} */
-        const _exhaustiveCheck = state;
-    }
-  }
-
-  /**
-   * @param {string} data
-   */
-  function parseData(data) {
-    if (!data || !DATA_REGEX.test(data)) throw new Error('Bad data');
-
-    const header = data.slice(0, 8);
-    const encoded = data.slice(8);
-
-    const encoding = /** @type {('2' | 'Z')} */ (header[2]);
-
-    const splitMod = 8;
-
-    const { version } = BBQr.findBestVersion(encoded.length, splitMod, {
-      encoding: encoding,
-      minSplit: 1,
-      maxSplit: 1,
-      minVersion: 1,
-      maxVersion: 40,
-    });
 
     return {
-      encoding,
-      splitMod,
-      encoded,
-      qrVersion: version,
+      getState: () => _state,
+      setState,
     };
-  }
+  })();
 
-  function downloadQR() {
+  async function downloadQR() {
+    const state = getState();
+
+    if (state.status !== 'loaded') {
+      console.warn('downloadQR called but state is not loaded');
+      return;
+    }
+
     const imgEl = qrCodeEl.querySelector('img');
     if (!imgEl) return;
     const url = imgEl.src;
@@ -114,17 +150,16 @@
     a.href = url;
 
     // make the filename KT_<type>_<16 random hex bytes>.png
-    let filename = 'KT_';
+    let filename = `KT_${state.qrInfo.fileType}_`;
 
-    const data = imgEl.getAttribute('data-data');
-
-    if (data && data.length >= 4) {
-      filename += data[3] + '_';
-    }
-
-    filename += Array.from(crypto.getRandomValues(new Uint8Array(16)))
+    const hash = await crypto.subtle.digest('SHA-256', state.qrInfo.rawBytes);
+    const hashBytes = new Uint8Array(hash);
+    const lastBytes = hashBytes.slice(-16);
+    filename += Array.from(lastBytes)
       .map((b) => b.toString(16).padStart(2, '0'))
       .join('');
+
+    filename += `_${state.qrInfo.qrParts.length}x`;
 
     filename += '.png';
 
@@ -133,34 +168,107 @@
     a.remove();
   }
 
-  async function run() {
+  async function loadDataFromUrl() {
     const data = window.location.hash ? window.location.hash.slice(1) : null;
 
     if (!data) {
-      updatePageState({ status: 'no-hash' });
+      setState({ status: 'no-hash' });
       return;
     }
 
-    try {
-      const { qrVersion } = parseData(data);
-      const imgBuf = await BBQr.renderQRImage([data], qrVersion, {
-        renderOptions: {
-          scale: 8,
-        },
+    if (!DATA_REGEX.test(data)) {
+      setState({ status: 'invalid-hash' });
+      return;
+    }
+
+    const header = data.slice(0, 8);
+
+    // validated by regex above
+    const encoding = /** @type {('2' | 'Z')} */ (header[2]);
+    const fileType = /** @type {('S' | 'R' | 'E')} */ (header[3]);
+
+    const { version } = BBQr.findBestVersion(data.slice(8).length, 8, {
+      encoding,
+      minSplit: 1,
+      maxSplit: 1,
+      minVersion: 1,
+      maxVersion: 40,
+    });
+
+    // "join" back to raw bytes, so we can adjust number of QRs etc.
+    const { raw } = BBQr.joinQRs([data]);
+
+    setState({
+      status: 'loaded',
+      qrInfo: { qrParts: [data], rawBytes: raw, encoding, fileType, qrVersion: version },
+    });
+  }
+
+  /**
+   * @param {'up' | 'down' | 'single'} action
+   */
+  async function adjustQRCount(action) {
+    const state = getState();
+
+    if (state.status !== 'loaded') {
+      console.warn('increaseQRCount called but state is not loaded');
+      return;
+    }
+
+    if (action === 'single') {
+      const { parts, version } = BBQr.splitQRs(state.qrInfo.rawBytes, state.qrInfo.fileType, {
+        encoding: state.qrInfo.encoding,
+        minSplit: 1,
+        maxSplit: 1,
+        minVersion: 1,
+        maxVersion: 40,
       });
 
-      const url = URL.createObjectURL(new Blob([imgBuf], { type: 'image/png' }));
-
-      updatePageState({ status: 'qr-code', url, data });
-    } catch (err) {
-      updatePageState({ status: 'invalid-hash' });
+      setState({
+        status: 'loaded',
+        qrInfo: { ...state.qrInfo, qrParts: parts, qrVersion: version },
+      });
       return;
+    }
+
+    const currentCount = state.qrInfo.qrParts.length;
+
+    let newCount = currentCount;
+
+    while (newCount > 0 && newCount <= 1295) {
+      newCount += action === 'up' ? 1 : -1;
+      try {
+        const { parts, version } = BBQr.splitQRs(state.qrInfo.rawBytes, state.qrInfo.fileType, {
+          encoding: state.qrInfo.encoding,
+          minSplit: newCount,
+          maxSplit: newCount,
+          minVersion: 1,
+          maxVersion: 40,
+        });
+
+        setState({
+          status: 'loaded',
+          qrInfo: { ...state.qrInfo, qrParts: parts, qrVersion: version },
+        });
+        return;
+      } catch (err) {
+        console.debug(`adjustQRCount: cannot split into ${newCount} parts, skipping`);
+      }
     }
   }
 
-  run();
+  loadDataFromUrl();
 
-  window.addEventListener('hashchange', run);
+  window.addEventListener('hashchange', loadDataFromUrl);
+
+  // countInputEl.addEventListener('change', (e) => {
+  //   const count = parseInt(countInputEl.value);
+  //   if (isNaN(count)) {
+  //     console.warn('setQRCount called with invalid count', count);
+  //     return;
+  //   }
+  //   setQRCount(count);
+  // });
 
   document.querySelectorAll('[data-action]').forEach((el) => {
     el.addEventListener('click', (e) => {
@@ -182,6 +290,12 @@
           });
       } else if (action === 'download-qr') {
         downloadQR();
+      } else if (action === 'more-frames') {
+        adjustQRCount('up');
+      } else if (action === 'less-frames') {
+        adjustQRCount('down');
+      } else if (action === 'no-animation') {
+        adjustQRCount('single');
       }
     });
   });
